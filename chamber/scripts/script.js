c14ee7480b58035c602e0721e7d7e49bc26939e4
dazoantsy <dazoantsy@gmail.com>
2023-06-19 17:29:42 +0300

    chamber update and activity w08

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -26,3 +26,65 @@ function toggleMenu() {
 
 const x = document.getElementById("hamburgerBtn");
 x.onclick = toggleMenu;
+
+
+//Message that appears on Mondays and Tuesdays
+let message
+if (weekday === 1 || weekday === 2) {
+    message = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
+}
+document.querySelector('#message').textContent = message;
+
+//Lazy Images
+const imagesToLoad = document.querySelectorAll('img[data-src]');
+
+const imgOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px 50px 0px"
+};
+
+const loadImages = (image) => {
+  image.setAttribute('src', image.getAttribute('data-src'));
+  image.onload = () => {
+    image.removeAttribute('data-src');
+  };
+};
+
+if('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+      items.forEach((item) => {
+        if(item.isIntersecting) {
+          loadImages(item.target);
+          observer.unobserve(item.target);
+        }
+      });
+    }, imgOptions);
+    imagesToLoad.forEach((img) => {
+      observer.observe(img);
+    });
+  } else {
+    imagesToLoad.forEach((img) => {
+      loadImages(img);
+    });
+  }
+
+  //Days between visits
+  let time = today.getTime();
+  let timeStored;
+  if(!localStorage.getItem('nbrOfDays')) {
+    AddToStorage();
+  } else {
+      TimeBetween();
+      AddToStorage();
+  }
+
+  function AddToStorage() {
+    localStorage.setItem('nbrOfDays', time);
+  }
+
+  function TimeBetween() {
+    let timeBtwn = time - localStorage.getItem('nbrOfDays');
+    let DaysNbr = Math.round(timeBtwn/(24*3600*1000));
+    let last_visit = document.getElementById("last_visit");
+    last_visit.innerHTML = `Last visit: ${DaysNbr} day(s)`;
+  }
